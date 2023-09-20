fc9b235a5e60813d93e278ac0f86eaf5585d12bd
giliking <chrisogili12@gmail>
2023-09-19 18:36:02 -0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
+  dist: {
     // Define environment variables here
     define: {
       'import.meta.env.VITE_APP_TMDB_API_KEY': process.env.VITE_APP_TMDB_API_KEY,
